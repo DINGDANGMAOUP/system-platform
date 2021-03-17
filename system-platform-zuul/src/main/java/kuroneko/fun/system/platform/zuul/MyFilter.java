0296dfb9d8546df193646016a3785b045068ee2b
kuroneko <964621334@qq.com>
2021-03-17 16:30:41 +0800

    **处理

--- system-platform-zuul/src/main/java/kuroneko/fun/system/platform/zuul/MyFilter.java
+++ system-platform-zuul/src/main/java/kuroneko/fun/system/platform/zuul/MyFilter.java
@@ -1,11 +1,17 @@
 package kuroneko.fun.system.platform.zuul;
 
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author kuroneko
+ */
 @Component
 public class MyFilter extends ZuulFilter {
 
@@ -29,22 +35,22 @@ public class MyFilter extends ZuulFilter {
   @Override
   public Object run() throws ZuulException {
     //        // filter需要执行的具体操作
-    //        RequestContext ctx = RequestContext.getCurrentContext();
-    //        HttpServletRequest request = ctx.getRequest();
-    //        String token = request.getParameter("token");
-    //        System.out.println(token);
-    //        if(token==null){
-    //            log.warn("there is no request token");
-    //            ctx.setSendZuulResponse(false);
-    //            ctx.setResponseStatusCode(401);
-    //            try {
-    //                ctx.getResponse().getWriter().write("there is no request token");
-    //            } catch (IOException e) {
-    //                e.printStackTrace();
-    //            }
-    //            return null;
-    //        }
-    //        log.info("ok");
+            RequestContext ctx = RequestContext.getCurrentContext();
+            HttpServletRequest request = ctx.getRequest();
+            String token = request.getParameter("token");
+            System.out.println(token);
+            if(token==null){
+                log.warn("there is no request token");
+                ctx.setSendZuulResponse(false);
+                ctx.setResponseStatusCode(401);
+                try {
+                    ctx.getResponse().getWriter().write("there is no request token");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            log.info("ok");
     return null;
   }
 }
